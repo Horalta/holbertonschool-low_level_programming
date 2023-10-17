d32fc8282b83d3d2797e69987c3bc924d4822355
Horalta <horace053game@gmail.com>
2023-10-17 00:52:28 -0700

    Tasks

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,17 +1,23 @@
 #include "main.h"
 
 /**
- * main - program entry point
+ * main - prints putchar
  *
- * Return: 0
+ * Return: Always 0
  */
 int main(void)
 {
-	int i = 0;
-	char str[] = "Holberton\n";
-
-	for (i = 0; i < 10; i++)
-		_putchar(str[i]);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
+
+
 }
