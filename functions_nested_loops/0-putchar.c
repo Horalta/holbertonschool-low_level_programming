833c4e7b4c94616bab92a8b9085f9990064fe6fa
Horalta <horace053game@gmail.com>
2023-10-17 00:57:15 -0700

    Tasks

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
- * main - prints putchar
+ * main - Prints Holberton as a message.
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	int str[] = {80, 85, 85, 67, 72, 65, 82};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
 	_putchar('\n');
-
 	return (0);
-
-
 }
