5307b40f1157e1011d0d69e0e43f5c22456ed204
Horalta <horace053game@gmail.com>
2023-10-17 00:45:35 -0700

    Tasks

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,1 +1,17 @@
+#include "main.h"
 
+/**
+ * main - program entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	char str[] = "Holberton\n";
+
+	for (i = 0; i < 10; i++)
+		_putchar(str[i]);
+
+	return (0);
+}
