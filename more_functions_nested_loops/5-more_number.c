82d4813d751dc205848ee32222ade589c13ef490
Horalta <146714266+Horalta@users.noreply.github.com>
2023-12-04 01:37:01 +0100

    Tasks

--- more_functions_nested_loops/5-more_number.c
+++ more_functions_nested_loops/5-more_number.c
@@ -1,26 +1,22 @@
 #include "main.h"
 
 /**
-  * more_numbers - Print 10 times the numbers since 0 up to 14
-  *
-  * Return: 10 times of the numbers since 0 up to 14
-  */
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ *
+ * Return: void
+ */
 void more_numbers(void)
 {
-	int x, y;
+	int i, j;
 
-	for (x = 0; x < 10; x++)
+	for (i = 0; i < 10; i++)
 	{
-		for (y = 0; y <= 14; y++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (y > 9)
-			{
-				_putchar((y / 10) + '0');
-			}
-
-			_putchar((y % 10) + '0');
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
-
 		_putchar('\n');
 	}
 }
